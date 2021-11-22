95c75b4abd0286ce30b266ccffe85490a41013a0
Michal <m.kotecki96@o2.pl>
2021-11-22 21:06:37 +0100

    drugi commit

---
+++ basics.py
@@ -0,0 +1,11 @@
+# na start - zainicjować projekt django:
+# django-admin startproject <name>
+# tworzy się nowy folder
+
+# dalej - python manage.py
+# python manage.py runserver - ta komenda uruchamia serwer django
+
+# dalej - python manage.py startapp <name> - rozpoczęcie tworzenia nowej aplikacji
+# w pliku settings.py trzeba wpisać nazwę utworzonego folderu do listy INSTALLED_APPS
+# w pliku urls.py (w folderze intro) trzeba stworzyć dyspozytor
+# w folderze views.py (hello) trzeba wkleić funkcję z views.py (intro)
